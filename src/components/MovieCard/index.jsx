747b25cef9a9a3af19a5de64183fe1e64e88c8ca
Hilman Azfar <m.hilman.azfar@gmail.com>
2020-09-12 22:39:35 +0800

    completed api call

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 export default function MovieCard(props) {
 
-  const { show : {name, image : {medium: src }}} = props.data
+  const {show : {name, image}} = props.data
 
   return (
     <Grid item xs={3} align="center">
-      <img src={src} 
-            alt={name}
-            style={{maxWidth: "150px"}}/>
+      <img src={image ? image.medium : "no-url"} 
+           alt={name}
+           style={{maxWidth: "150px"}}/>
       <Typography variant="h6" noWrap>{name}</Typography>
     </Grid>
   )
